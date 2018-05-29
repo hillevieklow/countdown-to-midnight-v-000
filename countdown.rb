701d51bb648e914a78054235eff587d7a5c997d5
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-29 19:22:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,18 @@
 #write your code here
 
-def countdown(integer)
-  while integer > 0
-    puts "#{integer} SECOND(S)!"
-    integer -= 1
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(integer)
-  while integer > 0
-    puts "#{integer} SECOND(S)!"
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
     sleep(1)
-    integer -= 1
+    x -= 1
   end
   "HAPPY NEW YEAR!"
 end
